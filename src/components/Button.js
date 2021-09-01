c3d775cdcd45a5c155f6a53f1bba519f7d863ddb
HakimuddinChundawada <hc5637@srmist.edu.in>
2021-09-02 01:37:53 +0530

    created Button component

---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
+
+const STYLES = ["btn--primary", "btn--outlined"];
+
+const SIZES = ["btn--medium", "btn--large"];
+
+export const Button = ({
+  children,
+  type,
+  handleClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  const selectedButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+  const selectedButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+  console.log("children ->", children);
+  return (
+    <Link to="sign-up" className="btn-mobile">
+      <button
+        className={`btn ${selectedButtonStyle} ${selectedButtonSize}`}
+        onClick={handleClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
