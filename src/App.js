8e3604d02cb4efe4034fe1611c5aed975962921f
HakimuddinChundawada <hc5637@srmist.edu.in>
2021-09-03 00:36:38 +0530

    implemented all pages in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Navbar from "./components/Navbar";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import Signup from "./components/pages/Signup";
+import "./App.css";
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/services" component={Services} />
+          <Route path="/sign-up" component={Signup} />
         </Switch>
       </Router>
     </>
