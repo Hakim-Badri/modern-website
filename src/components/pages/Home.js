aeff9c66bcb1723cc5b5a1e29d382680413bd27b
HakimuddinChundawada <hc5637@srmist.edu.in>
2021-09-03 00:38:55 +0530

    added Hero and card sections to home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CardSection from "../CardSection";
+import HeroSection from "../HeroSection";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <CardSection />
+    </>
+  );
+}
+
+export default Home;
